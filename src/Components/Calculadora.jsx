f354ed2037df00f43a48e2f85549026f21b67905
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-05-26 16:38:35 -0500

    Calculadora

---
+++ src/Components/Calculadora.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NumberInput from './NumberInput'
+
+const Calculadora = () => {
+
+
+    return (
+        <div>
+            <NumberInput />
+
+        </div>
+    )
+}
+
+export default Calculadora
